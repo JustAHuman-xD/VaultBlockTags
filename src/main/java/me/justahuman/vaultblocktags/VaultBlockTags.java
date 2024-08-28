0bf7fae8c431614c826b7d601ae8a06fb6388434
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-28 15:33:44 -0500

    prep repo

---
+++ src/main/java/me/justahuman/vaultblocktags/VaultBlockTags.java
@@ -0,0 +1,14 @@
+package me.justahuman.vaultblocktags;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod("vaultblocktags")
+public class VaultBlockTags {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public VaultBlockTags() {
+
+    }
+}

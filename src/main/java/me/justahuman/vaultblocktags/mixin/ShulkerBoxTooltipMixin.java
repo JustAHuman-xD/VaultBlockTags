cc58ce4cad9c65740d6123e49fa2c37a9abde2fd
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-30 18:43:50 -0500

    test

--- src/main/java/me/justahuman/vaultblocktags/mixin/ShulkerBoxTooltipMixin.java
+++ src/main/java/me/justahuman/vaultblocktags/mixin/ShulkerBoxTooltipMixin.java
@@ -1,58 +1,32 @@
 package me.justahuman.vaultblocktags.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import iskallia.vault.block.VaultCrateBlock;
 import me.justahuman.vaultblocktags.VaultBlockTags;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.items.IItemHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import vazkii.quark.content.client.tooltip.ShulkerBoxTooltips;
 
 @Mixin(ShulkerBoxTooltips.ShulkerComponent.class)
 public class ShulkerBoxTooltipMixin {
 
-    @Unique
-    private static boolean logged = false;
-    @Shadow(remap = false) @Final private static int[][] TARGET_RATIOS;
-
     @Shadow @Final private ItemStack stack;
 
-    @Inject(method = "lambda$renderImage$0", at = @At("HEAD"), remap = false)
-    public void renderImage(int tooltipX, int tooltipY, Minecraft mc, PoseStack pose, IItemHandler capability, CallbackInfo ci) {
-        if (logged) {
-            return;
-        }
-
-        int size = capability.getSlots();
-        int[] dims = new int[]{Math.min(size, 9), Math.max(size / 9, 1)};
-        CompoundTag cmp = this.stack.getOrCreateTagElement("BlockEntityTag");
-        BlockEntity te = BlockEntity.loadStatic(BlockPos.ZERO, ((BlockItem)this.stack.getItem()).getBlock().defaultBlockState(), cmp);
-        VaultBlockTags.LOGGER.info(" ");
-        VaultBlockTags.LOGGER.info("bingo slots size: {}", size);
-        VaultBlockTags.LOGGER.info("bingo slots dims: {}", dims);
-        VaultBlockTags.LOGGER.info("blockstate: {}", te.getClass().getSimpleName());
-        VaultBlockTags.LOGGER.info("itemstack: {}", this.stack.serializeNBT().toString());
-        VaultBlockTags.LOGGER.info(" ");
-        for (int[] ratio : TARGET_RATIOS) {
-            VaultBlockTags.LOGGER.info("test ratio: {}", ratio);
-            if (ratio[0] * ratio[1] == size) {
-                VaultBlockTags.LOGGER.info("overriding dims with test");
-                VaultBlockTags.LOGGER.info(" ");
-                break;
+    @ModifyConstant(method = "renderImage", constant = @Constant(stringValue = "minecraft:shulker_box"), remap = false)
+    public String renderImage(String constant) {
+        if (stack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof VaultCrateBlock) {
+            ResourceLocation registryName = stack.getItem().getRegistryName();
+            if (registryName != null) {
+                return registryName.toString();
+            } else {
+                VaultBlockTags.LOGGER.info("null for some reason?");
             }
-            VaultBlockTags.LOGGER.info(" ");
         }
-        capability.getStackInSlot(28);
-        logged = true;
+        return constant;
     }
 }

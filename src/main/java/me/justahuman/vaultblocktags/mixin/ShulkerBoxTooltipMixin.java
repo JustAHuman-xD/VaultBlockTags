924db6b15aa9b714f15c94faa15fb36cd6032c1b
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-30 17:41:55 -0500

    debug messages

---
+++ src/main/java/me/justahuman/vaultblocktags/mixin/ShulkerBoxTooltipMixin.java
@@ -0,0 +1,44 @@
+package me.justahuman.vaultblocktags.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import iskallia.vault.init.ModBlocks;
+import me.justahuman.vaultblocktags.VaultBlockTags;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vazkii.quark.content.client.tooltip.ShulkerBoxTooltips;
+
+@Mixin(ShulkerBoxTooltips.ShulkerComponent.class)
+public class ShulkerBoxTooltipMixin {
+    @Shadow @Final private ItemStack stack;
+
+    @Shadow @Final private static int[][] TARGET_RATIOS;
+
+    @Inject(method = "lambda$renderImage$0", at = @At("HEAD"))
+    public void renderImage(int tooltipX, int tooltipY, Minecraft mc, PoseStack pose, IItemHandler capability, CallbackInfo ci) {
+        if (this.stack.getItem() == ModBlocks.VAULT_CRATE_BINGO_FULL.asItem()) {
+            int size = capability.getSlots();
+            int[] dims = new int[]{Math.min(size, 9), Math.max(size / 9, 1)};
+            VaultBlockTags.LOGGER.info(" ");
+            VaultBlockTags.LOGGER.info("bingo slots size: {}", size);
+            VaultBlockTags.LOGGER.info("bingo slots dims: {}", dims);
+            VaultBlockTags.LOGGER.info(" ");
+            for (int i = 0; i < TARGET_RATIOS.length; ++i) {
+                int[] ratio = TARGET_RATIOS[i];
+                VaultBlockTags.LOGGER.info("test ratio: {}", ratio);
+                if (ratio[0] * ratio[1] == size) {
+                    VaultBlockTags.LOGGER.info("overriding dims with test");
+                    VaultBlockTags.LOGGER.info(" ");
+                    break;
+                }
+                VaultBlockTags.LOGGER.info(" ");
+            }
+        }
+    }
+}

5f8b6ff42f541c05ac23b35d0a305f45fde83cb9
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-30 18:55:09 -0500

    alright this should hopefully work lol

--- src/main/java/me/justahuman/vaultblocktags/mixin/ShulkerBoxTooltipMixin.java
+++ src/main/java/me/justahuman/vaultblocktags/mixin/ShulkerBoxTooltipMixin.java
@@ -1,8 +1,6 @@
 package me.justahuman.vaultblocktags.mixin;
 
 import iskallia.vault.block.VaultCrateBlock;
-import me.justahuman.vaultblocktags.VaultBlockTags;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
@@ -20,12 +18,7 @@ public class ShulkerBoxTooltipMixin {
     @ModifyConstant(method = "renderImage", constant = @Constant(stringValue = "minecraft:shulker_box"))
     public String renderImage(String constant) {
         if (stack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof VaultCrateBlock) {
-            ResourceLocation registryName = stack.getItem().getRegistryName();
-            if (registryName != null) {
-                return registryName.toString();
-            } else {
-                VaultBlockTags.LOGGER.info("null for some reason?");
-            }
+            return "the_vault:vault_crate_tile_entity";
         }
         return constant;
     }

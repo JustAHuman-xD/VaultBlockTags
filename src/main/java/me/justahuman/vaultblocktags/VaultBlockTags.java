fd4a7fe4ddec9d207ba4fff46c6a5e805557952f
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-31 06:52:09 -0500

    :eyes:

--- src/main/java/me/justahuman/vaultblocktags/VaultBlockTags.java
+++ src/main/java/me/justahuman/vaultblocktags/VaultBlockTags.java
@@ -5,8 +5,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 import java.util.function.Function;
@@ -16,12 +20,15 @@ public class VaultBlockTags {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public VaultBlockTags() {
-        // this doesn't go here but wip
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::onClientSetup);
+        bus.addListener(this::makeTooltip);
+    }
+
+    public void onClientSetup(FMLClientSetupEvent event) {
         MinecraftForgeClient.registerTooltipComponentFactory(VaultCrateTooltips.CrateComponent.class, Function.identity());
     }
 
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
     public void makeTooltip(RenderTooltipEvent.GatherComponents event) {
         VaultCrateTooltips.makeTooltip(event);
     }

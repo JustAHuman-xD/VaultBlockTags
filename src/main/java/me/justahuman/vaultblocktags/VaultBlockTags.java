1929fb436f32d5bf9ab6d01bdb3efe1d3ed18e43
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-08-30 20:08:43 -0500

    nearly  done

--- src/main/java/me/justahuman/vaultblocktags/VaultBlockTags.java
+++ src/main/java/me/justahuman/vaultblocktags/VaultBlockTags.java
@@ -1,14 +1,28 @@
 package me.justahuman.vaultblocktags;
 
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
+
+import java.util.function.Function;
 
 @Mod("vaultblocktags")
 public class VaultBlockTags {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public VaultBlockTags() {
+        // this doesn't go here but wip
+        MinecraftForgeClient.registerTooltipComponentFactory(VaultCrateTooltips.CrateComponent.class, Function.identity());
+    }
 
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void makeTooltip(RenderTooltipEvent.GatherComponents event) {
+        VaultCrateTooltips.makeTooltip(event);
     }
 }
---
+++ src/main/java/me/justahuman/vaultblocktags/VaultCrateTooltips.java
@@ -0,0 +1,83 @@
+package me.justahuman.vaultblocktags;
+
+import com.mojang.blaze3d.platform.Window;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.datafixers.util.Either;
+import iskallia.vault.block.VaultCrateBlock;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import vazkii.quark.content.client.tooltip.ShulkerBoxTooltips;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public class VaultCrateTooltips {
+
+    @OnlyIn(Dist.CLIENT)
+    public static void makeTooltip(RenderTooltipEvent.GatherComponents event) {
+        ItemStack itemStack = event.getItemStack();
+        if (itemStack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof VaultCrateBlock && Screen.hasShiftDown()) {
+            CompoundTag pTag = itemStack.getTag();
+            if (pTag != null && pTag.contains("Items", 10)) {
+                NonNullList<ItemStack> itemStacks = NonNullList.withSize(pTag.getList("Items", 10).size(), ItemStack.EMPTY);
+                ContainerHelper.loadAllItems(pTag, itemStacks);
+                event.getTooltipElements().add(1, Either.right(new CrateComponent(itemStacks)));
+            }
+        }
+    }
+
+    public record CrateComponent(NonNullList<ItemStack> itemStacks) implements ClientTooltipComponent, TooltipComponent {
+
+        @Override
+        @ParametersAreNonnullByDefault
+        public void renderImage(Font font, int tooltipX, int tooltipY, PoseStack pose, ItemRenderer renderer, int pTicks) {
+            Minecraft minecraft = Minecraft.getInstance();
+            int currentX = tooltipX;
+            int currentY = tooltipY - 1;
+            int textWidth = 10 + 18 * 9;
+
+            int right = currentX + textWidth;
+            Window window = minecraft.getWindow();
+            if (right > window.getScreenWidth()) {
+                currentX -= right - window.getGuiScaledWidth();
+            }
+
+            pose.pushPose();
+            pose.translate(0, 0, 700);
+
+            ShulkerBoxTooltips.ShulkerComponent.renderTooltipBackground(minecraft, pose, currentX, currentY, 9, 6, -1);
+
+            for (int i = 0; i < itemStacks.size(); i++) {
+                ItemStack itemStack = itemStacks.get(i);
+                if (!itemStack.isEmpty()) {
+                    int xp = currentX + 6 + i % 9 * 18;
+                    int yp = currentY + 6 + i / 9 * 18;
+                    renderer.renderAndDecorateItem(itemStack, xp, yp);
+                    renderer.renderGuiItemDecorations(minecraft.font, itemStack, xp, yp);
+                }
+            }
+        }
+
+        @Override
+        public int getHeight() {
+            return 119;
+        }
+
+        @Override
+        public int getWidth(@Nonnull Font font) {
+            return 171;
+        }
+    }
+}

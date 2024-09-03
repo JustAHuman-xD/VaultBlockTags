b1a953c11732f8c4642d18733bb8368e7af00ecf
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-09-03 10:59:38 -0500

    re-add loot manager mixin

---
+++ src/main/java/me/justahuman/vaultblocktags/mixin/minecraft/LootManagerMixin.java
@@ -0,0 +1,21 @@
+package me.justahuman.vaultblocktags.mixin.minecraft;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.loot.LootManager;
+import net.minecraft.loot.LootTableReporter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(LootManager.class)
+public class LootManagerMixin {
+    @Unique
+    private static final Multimap<String, String> DUMMY_MAP = ImmutableMultimap.of();
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/loot/LootTableReporter;getMessages()Lcom/google/common/collect/Multimap;"), method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V")
+    public Multimap<String, String> getMessages(LootTableReporter instance) {
+        return DUMMY_MAP;
+    }
+}
